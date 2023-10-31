5172754f70a916f234ca956f5043b291dd2d38f0
TU-PJY <viska0319@icloud.com>
2023-10-31 15:41:39 +0900

    Merged modelCounts.h and modeEnum.g into GL_config.h

---
+++ GL_kit/header/GL_config.h
@@ -0,0 +1,13 @@
+// 모드 열거형
+#ifndef MODE_ENUM_H
+#define MODE_ENUM_H
+#define MODEL_COUNTS 0
+
+enum bufferMode {
+	modeInit, modeUpdate
+};
+enum projectionMode {
+	modeOrtho, modePers
+};
+
+#endif
